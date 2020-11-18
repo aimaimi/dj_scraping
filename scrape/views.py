31744ab8c1445bb59a3ecd13900a17bc57e2d521
aimaimi <yh1127@hotmail.co.jp>
2020-11-18 15:55:43 +0900

    Django管理サイト実装

--- scrape/views.py
+++ scrape/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-  name = "scrapoo"
+  name = f"scrapoo {datetime.today()}"
   html = f"<h1>Hello {name}</h1>"
   return HttpResponse(html)
